6c297d22d1b1086db7e05cbdb7d717c0911aa62a
Bill Ragsdale <bill@billragsdale.cc>
2023-12-08 15:18:56 -0800

    CTtoFloat.f

---
+++ CTtoFloat.f
@@ -0,0 +1,4 @@
+# FloatConversions
+Floating Point Conversions
+Thse Win32Forth words  convert the ancient MicroSoft QBasic 32 bit floating point format into IEEE 754 Floating Point 
+and fromn IEEE to MicroSoft floats.
--- README.md
+++
@@ -1,2 +0,0 @@
-# FloatConversions
-Floating Point Conversions
